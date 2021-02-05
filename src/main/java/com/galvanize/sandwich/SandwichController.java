11fbc0b11a0557ed3c79ba66f092ae5241313383
Jonathan Lin <jontitan@gmail.com>
2021-02-04 20:56:04 -0600

    Implement add sandwich endpoint

--- src/main/java/com/galvanize/sandwich/SandwichController.java
+++ src/main/java/com/galvanize/sandwich/SandwichController.java
@@ -1,14 +1,18 @@
 package com.galvanize.sandwich;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class SandwichController {
-    private SandwichRepository repository;
+    private final SandwichRepository repository;
 
     @Autowired
     public SandwichController(SandwichRepository repository) {
@@ -23,5 +27,11 @@ public class SandwichController {
     @GetMapping("/sandwiches")
     public List<Sandwich> getAllSandwiches() {
         return repository.findAll();
+    }
+
+    @PostMapping("/sandwiches")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void add(@RequestBody Sandwich sandwich) {
+        repository.save(sandwich);
     }
 }
--- src/test/java/com/galvanize/sandwich/SandwichControllerTest.java
+++ src/test/java/com/galvanize/sandwich/SandwichControllerTest.java
@@ -1,12 +1,16 @@
 package com.galvanize.sandwich;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,12 +21,30 @@ public class SandwichControllerTest {
     MockMvc mvc;
 
     @Test
-    void testGetAllSandwiches() throws Exception {
+    void getAllSandwiches() throws Exception {
         mvc.perform(get("/sandwiches"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("[0].name").value("Ham and Cheese"))
-                .andExpect(jsonPath("[1].name").value("Turkey and Swiss"))
-                .andExpect(jsonPath("[2].name").value("Veggie Deluxe"))
-                .andExpect(jsonPath("[3].name").value("Kale and Lime"));
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("[0].name").value("Ham and Cheese"))
+            .andExpect(jsonPath("[1].name").value("Turkey and Swiss"))
+            .andExpect(jsonPath("[2].name").value("Veggie Deluxe"))
+            .andExpect(jsonPath("[3].name").value("Kale and Lime"));
+    }
+
+    @Test
+    void AddSandwich() throws Exception {
+        Sandwich chickenClub = new Sandwich();
+        chickenClub.setName("Chicken Club");
+
+        mvc.perform(
+            post("/sandwiches")
+                .content(new ObjectMapper().writeValueAsString(chickenClub))
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isCreated());
+
+        mvc.perform(get("/sandwiches"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$", hasSize(5)))
+            .andExpect(jsonPath("[4].name").value("Chicken Club"));
     }
 }
