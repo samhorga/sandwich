8d64fb546f91990f8e6ad94b0a94a1c915902221
Samuel Horga <samuel.horga@cognizant.com>
2021-02-05 12:13:27 -0700

    Added schema.sql file, and changed appplication.prop to application-postgres.properties

--- src/main/java/com/galvanize/sandwich/Sandwich.java
+++ src/main/java/com/galvanize/sandwich/Sandwich.java
@@ -7,14 +7,14 @@ import javax.persistence.*;
 public class Sandwich {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
     private String name;
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS sandwich;
+CREATE TABLE sandwich
+(
+    id serial NOT NULL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL
+);
